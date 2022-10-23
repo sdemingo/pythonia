d7e82e6e4f6bce011018b2481e33de52fe1a55f7
Sergio de Mingo <sdemingo@gmail.com>
2022-10-23 12:46:55 +0200

    firsts test

--- pythonia.py
+++ pythonia.py
@@ -2,23 +2,52 @@ import turtle
 
 
 
-def cal_intersection(a1,b1,a2,b2):
-    try:
-        x = (b2-b1)/(a1-a2)
-        y = a1*(b2-b1)/(a1-a2) + b1
-        return (x,y)
-    except Exception as e:
-        print(str(e))
-        return None
+# def cal_intersection(a1,b1,a2,b2):
+#     try:
+#         x = (b2-b1)/(a1-a2)
+#         y = a1*(b2-b1)/(a1-a2) + b1
+#         return (x,y)
+#     except Exception as e:
+#         print(str(e))
+#         return None
 
 
+def isCollision2(t1, t2):
+        if abs (t1.xcor () - t2.xcor ()) < 20 :
+            a = t1.ycor ()
+            b = t2.ycor ()
+            if a < b and a > b - 400 :
+                return True
+        else:
+            return False
+
+
+def isCollision(t1,t2):
+    if t1.distance(t2) < 10:
+        return True
+    else:
+        return False
+        
+        
 class Hero (turtle.Turtle): 
     def __init__(self,x,y):
-        super(Hero,self).__init__(  )
+        super(Hero,self).__init__( )
         self.pu()
         self.goto(x,y)
         self.pd()
 
+        
+class Area (turtle.Turtle):
+    def __init__(self,x,y):
+        super(Area,self).__init__()
+        self.x=x
+        self.y=y
+        self.shape("square")
+        self.pu()
+        
+    def draw(self):
+        self.goto(self.x, self.y)
+        
 
 class Wall:
     def __init__(self,x0,y0,x1,y1):
@@ -38,15 +67,19 @@ class Wall:
         self.wall.goto(self.x1,self.y1)
 
 
-wall1=Wall(100,-50,100,200)
-wall1.draw()
+
+def main():
+
+    area1=Area(40,40)
+    area1.draw()
+    
+    hero = Hero(40,40)
+    hero.fd(20)
+
+    print (isCollision(hero,area1))
 
 
+    turtle.mainloop()    
 
-hero = Hero(10,100)
-hero.fd(150)
-
-
-
-
-turtle.mainloop()
+if __name__ == '__main__':
+    main()
