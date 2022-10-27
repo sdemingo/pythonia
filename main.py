70ecb8de7501706f3189aadca3173650f084b335
Sergio de Mingo <sdemingo@gmail.com>
2022-10-27 09:37:28 +0200

    reuse the same frame during reload

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 ## Recuerda que tu personaje se llama 'hero'
 ##
 ## Por ejemplo usa:  hero.fd(100)
+##
+## Para recargar tu código pulsa 'r' sobre la ventana
 
 
 hero.fd(100)
-
+hero.rt(90)
+hero.fd(200)
 
--- turtle_core.py
+++ turtle_core.py
@@ -1,10 +1,47 @@
 
 from turtle import *
 import os
+import signal
 
 hero=Turtle()
+titles=Turtle()
 
-exec(open('main.py').read())
+def reload_script():
+    hero.reset()
+    titles.reset()
 
-mainloop()
+    titles.hideturtle()
+    titles.penup()
+    titles.goto(-280,280)
+    titles.write("Pulsa F5 para actualizar", move=False, align="left")
+    
+    
+    exec(open('main.py').read())
+
+
+def handler(signum, frame):
+    print ("Cierra la ventana para cerrar la aplicación")
+
+
+def run_editor():
+    os.system("gedit main.py &")
+    
+
+if __name__=='__main__':
+
+    
+    wn = Screen()
+    setup(600, 600,0,0)
+
+    reload_script()
+    # run_editor()
+
+    wn.onkey(reload_script,'F5')
+
+    signal.signal(signal.SIGINT, handler)
+    try:
+        wn.listen()
+        wn.mainloop()
+    except KeyboarInterrupt as err:
+        print (err)
 
