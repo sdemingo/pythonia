3dfac54b63366558f358a4e90cdd18bfc0919bb6
Sergio de Mingo <sdemingo@gmail.com>
2022-10-28 10:17:21 +0200

    working on button bar

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-
 ## Escribe aquí el código para mover tu personaje.
 ## Recuerda que tu personaje se llama 'hero'
 ##
 ## Por ejemplo usa:  hero.fd(100)
-##
-## Para recargar tu código pulsa 'r' sobre la ventana
 
-
-hero.fd(100)
-hero.rt(90)
+hero.fd(200)
+hero.lt(90)
 hero.fd(200)
 
--- turtle_core.py
+++ turtle_core.py
@@ -1,43 +1,65 @@
 
 from turtle import *
+from tkinter import Button,filedialog
 import os
 import signal
 
 hero=Turtle()
 titles=Turtle()
 
+HEIGHT=500
+WIDTH=500
+
+main_script="main.py"
+
 def reload_script():
     hero.reset()
-    titles.reset()
-
-    titles.hideturtle()
-    titles.penup()
-    titles.goto(-280,280)
-    titles.write("Pulsa F5 para actualizar", move=False, align="left")
-    
-    
+    # titles.reset()
+    # titles.hideturtle()
+    # titles.penup()
+    # titles.goto(-280,280)
+    # titles.write("Pulsa F5 para actualizar", move=False, align="left")
     exec(open('main.py').read())
 
 
+def open_script():
+    filename = filedialog.askopenfilename()
+    print ("Quieres abrir el fichero "+str(filename))
+
+
+
+def edit_script():
+    os.system("gedit "+main_script+" &")
+
+    
+def build_ui(screen):
+    screen.title("Pythonia 0.1")
+    canvas = screen.getcanvas()
+
+    buttonOpen = Button(canvas.master, text="Open", height=1, width=5,  command=open_script)
+    buttonEdit = Button(canvas.master, text="Edit", height=1, width=5, command=edit_script)
+    buttonRun = Button(canvas.master, text="Run", height=1, width=5, command=reload_script)
+    
+    buttonOpen.place(x=5, y=5)
+    buttonEdit.place(x=75, y=5)
+    buttonRun.place(x=145, y=5)
+
+    screen.onkey(reload_script,'F5')
+
+
+    
 def handler(signum, frame):
     print ("Cierra la ventana para cerrar la aplicación")
 
 
-def run_editor():
-    os.system("gedit main.py &")
-    
-
 if __name__=='__main__':
 
-    
     wn = Screen()
-    setup(600, 600,0,0)
+    setup(WIDTH, HEIGHT,0,0)
+    build_ui(wn)
 
     reload_script()
-    # run_editor()
-
-    wn.onkey(reload_script,'F5')
-
+    
     signal.signal(signal.SIGINT, handler)
     try:
         wn.listen()
