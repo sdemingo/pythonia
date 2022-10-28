309f4284744ae5d2f8f11083ce7962b48cdf8645
Sergio de Mingo <sdemingo@gmail.com>
2022-10-28 20:07:59 +0200

    update gui

--- turtle_core.py
+++ turtle_core.py
@@ -1,6 +1,6 @@
 
 from turtle import *
-from tkinter import Button,filedialog
+from tkinter import Button,Label,filedialog
 import os
 import signal
 
@@ -12,21 +12,15 @@ WIDTH=500
 
 main_script="main.py"
 
+
 def reload_script():
     hero.reset()
-    # titles.reset()
-    # titles.hideturtle()
-    # titles.penup()
-    # titles.goto(-280,280)
-    # titles.write("Pulsa F5 para actualizar", move=False, align="left")
     exec(open('main.py').read())
 
 
 def open_script():
     filename = filedialog.askopenfilename()
     print ("Quieres abrir el fichero "+str(filename))
-
-
 
 def edit_script():
     os.system("gedit "+main_script+" &")
@@ -35,15 +29,21 @@ def edit_script():
 def build_ui(screen):
     screen.title("Pythonia 0.1")
     canvas = screen.getcanvas()
-
+    canvas.master.minsize(WIDTH,HEIGHT)
+    
+    filename_script=os.path.basename(main_script)
     buttonOpen = Button(canvas.master, text="Open", height=1, width=5,  command=open_script)
     buttonEdit = Button(canvas.master, text="Edit", height=1, width=5, command=edit_script)
     buttonRun = Button(canvas.master, text="Run", height=1, width=5, command=reload_script)
+    labelFile = Label(canvas.master, text=filename_script, bg="#fff")
+    labelError = Label(canvas.master, text="Mensaje de error", bg="#fff")
     
     buttonOpen.place(x=5, y=5)
     buttonEdit.place(x=75, y=5)
     buttonRun.place(x=145, y=5)
-
+    labelFile.place(x=215,y=10)
+    labelError.place(x=10,y=50)
+    
     screen.onkey(reload_script,'F5')
 
 
